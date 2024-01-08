25a6dc0d225f3ecc430b3adff94e6f006c680e29
HSYN <huseyn.mirzayev@code.edu.az>
2024-01-08 23:05:19 +0400

    task finish :)

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const result = document.getElementById("result");
+
+const input = document.getElementById("input");
+const btn = document.getElementById("search__btn");
+
+const user = document.getElementById("user");
+const username = document.getElementById("username");
+const joined = document.getElementById("date");
+const repos = document.getElementById("repo");
+const followers = document.getElementById("follower");
+const followings = document.getElementById("following");
+const bio = document.getElementById("bio");
+const img = document.getElementById("img");
+
+const repo__links = document.getElementById("repo__links");
+
+const loading = document.getElementById("loading");
+const errorEl = document.getElementById("error");
+
+let dev;
